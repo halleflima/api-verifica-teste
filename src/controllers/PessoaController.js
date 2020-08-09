885d68e79d4d99318410defd58dc4bebb087c04c
Hallef Lima <halleflima@hotmail.com>
2020-08-09 11:52:55 -0300

    Retorna token na criação do user

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -1,36 +1,49 @@
-const Pessoas = require('../models/Pessoas.js')
+const Pessoas = require('../models/Pessoas.js');
+
+const jwt = require('jsonwebtoken');
 
 module.exports = {
   async store(req, res, next) {
-    const { nome, nivel_user, email, senha   } = req.body
+    try {
+      const { nome, nivel_user, email, senha } = req.body;
 
-    const pessoa = await Pessoas.create({
-      nome,
-      nivel_user,
-      email,
-      senha
-    })
+      const pessoa = await Pessoas.create({
+        nome,
+        nivel_user,
+        email,
+        senha,
+      });
 
-    return res.json(pessoa)
+      pessoa.senha = undefined;
+
+      const token = jwt.sign(
+        { pessoa: pessoa.id },
+        'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwi'
+      );
+
+      return res.json({ pessoa, token });
+    } catch (error) {
+      return res.json(400, error);
+    }
   },
 
   async index(req, res, next) {
-    const { idpessoa } = req.params
-    const { email, senha } = req.body
-    
-    const pessoa = await Pessoas.findByPk(idpessoa)
+    const { idpessoa } = req.params;
+    const { email, senha } = req.body;
+
+    const pessoa = await Pessoas.findByPk(idpessoa);
     if (!pessoa) {
       return res.status(400).json({ error: 'Usuario não encontrado' });
     }
-    if (pessoa.email !==  email) {
+    if (pessoa.email !== email) {
       return res.status(400).json({ error: 'Nome de usuario errado!' });
     }
 
-    const checa_senha = await pessoa.checarSenha(senha)
+    const checa_senha = await pessoa.checarSenha(senha);
     if (!checa_senha) {
       return res.status(400).json({ error: 'Senha incorreta' });
-    }else{
+    } else {
       return res.status(200).json({ msg: 'Autenticação feita!' });
     }
-  }
-}
+  },
+};
